c56c345541a513e9c561673f11f12d8afc7d7b4a
PallabkumarS <pallabkumar26@gmail.com>
2023-12-11 23:56:43 +0600

    trying next ui with next js

---
+++ src/pages/app.js
@@ -0,0 +1,11 @@
+import { NextUIProvider } from "nextui";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
+}
+
+export default MyApp;
